b48439649fc10f90e07bcf824d710317e6f6e2fb
zbrachinara <zbrachinara@gmail.com>
2022-01-27 05:51:16 -0500

    added table of chips

--- hardware_simulator/src/parser.rs
+++ hardware_simulator/src/parser.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
 
+static CHIP_TABLE: Arc<RwLock<HashMap<String, Chip>>> = Arc::new(RwLock::new(HashMap::new()));
 
 struct Chip {
     in_pins: Vec<String>,
@@ -7,5 +10,9 @@ struct Chip {
 
 struct Instruction {
     chip_name: String,
-    inputs: Vec<bool>
+    inputs: Vec<bool>,
+}
+
+fn parse_instruction() -> Instruction {
+    todo!()
 }
