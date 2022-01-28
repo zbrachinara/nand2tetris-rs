1d17939ace792ca5881c5f25fc4855d7ae549790
Eric Yu <zbrachinara@gmail.com>
2022-01-27 21:42:35 -0500

    add symbol creation from string

--- hardware_simulator/src/parser.rs
+++ hardware_simulator/src/parser.rs
@@ -3,10 +3,12 @@ use nom::branch::alt;
 use nom::bytes::complete::{is_not, take, take_till};
 use nom::combinator::{opt, rest};
 use nom::sequence::tuple;
-use nom::IResult;
 use nom::Parser;
+use nom::{IResult, InputIter};
+use std::collections::btree_map::IntoValues;
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
+use thiserror::Error;
 
 lazy_static! {
     static ref CHIP_TABLE: Arc<RwLock<HashMap<String, Chip>>> =
@@ -42,6 +44,33 @@ enum Symbol<'a> {
     Number(usize),
 }
 
+#[derive(Error, Debug, PartialEq)]
+pub enum ParseError {
+    #[error("Symbol `{0}` is not a valid symbol")]
+    BadSymbol(String),
+}
+
+impl<'a> TryFrom<&'a str> for Symbol<'a> {
+    type Error = ParseError;
+
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
+        // a valid symbol must be in only ascii characters, as well as consisting of no whitespace
+        if value.is_ascii() && value.chars().all(|c| !c.is_ascii_whitespace()) {
+            Ok(if let Ok(num) = usize::from_str_radix(value, 10) {
+                Symbol::Number(num)
+            } else {
+                match value {
+                    "true" => Symbol::Value(Value::True),
+                    "false" => Symbol::Value(Value::False),
+                    x => Symbol::Name(x),
+                }
+            })
+        } else {
+            Err(ParseError::BadSymbol(String::from(value)))
+        }
+    }
+}
+
 fn parse_arg(arg: &str) -> nom::IResult<&str, Argument> {
     let (remainder, (internal, _, external, _)) = tuple((
         // get the first name
@@ -67,7 +96,18 @@ fn parse_instruction(_: &str) -> nom::IResult<Instruction, &str> {
 
 #[cfg(test)]
 mod test {
+    use crate::parser::ParseError::BadSymbol;
     use super::*;
+
+    #[test]
+    fn create_symbol() {
+
+        assert_eq!(Symbol::try_from("breh"), Ok(Symbol::Name("breh")));
+        assert_eq!(Symbol::try_from("12345"), Ok(Symbol::Number(12345)));
+        assert_eq!(Symbol::try_from("false"), Ok(Symbol::Value(Value::False)));
+        assert!(matches!(Symbol::try_from("u r bad"), Err(ParseError::BadSymbol(_))));
+
+    }
 
     #[test]
     fn test_parse_arg() {
