cd5fa4285f327f10251efc48ae304537649835d0
Eric Yu <zbrachinara@gmail.com>
2022-01-29 11:27:45 -0500

    moved types out of specific parsing module

--- hardware_simulator/src/parser/instruction.rs
+++ hardware_simulator/src/parser/instruction.rs
@@ -16,54 +16,7 @@ use nom::Parser;
 use nom::{Err, IResult};
 use thiserror::Error;
 
-lazy_static! {
-    static ref CHIP_TABLE: Arc<RwLock<HashMap<String, Chip>>> =
-        Arc::new(RwLock::new(HashMap::new()));
-}
-
-struct Chip {
-    in_pins: Vec<String>,
-    out_pins: Vec<String>,
-}
-
-#[derive(Eq, PartialEq, Debug)]
-struct Instruction<'a> {
-    chip_name: Symbol<'a>,
-    inputs: Vec<Argument<'a>>,
-}
-
-#[derive(Eq, PartialEq, Debug)]
-struct Argument<'a> {
-    internal: Symbol<'a>,
-    internal_bus: Option<BusRange>,
-    external: Symbol<'a>,
-    external_bus: Option<BusRange>,
-}
-
-#[derive(Eq, PartialEq, Debug)]
-enum Value {
-    True,
-    False,
-}
-
-#[derive(Eq, PartialEq, Debug)]
-enum Symbol<'a> {
-    Name(&'a str),
-    Value(Value),
-    Number(usize),
-}
-
-#[derive(Debug, Eq, PartialEq)]
-struct BusRange {
-    start: u16,
-    end: u16,
-}
-
-#[derive(Error, Debug, PartialEq)]
-pub enum HdlParseError<'a> {
-    #[error("Symbol `{0}` is not a valid symbol")]
-    BadSymbol(&'a str),
-}
+use super::*;
 
 impl<'a> TryFrom<&'a str> for Symbol<'a> {
     type Error = HdlParseError<'a>;
--- hardware_simulator/src/parser/mod.rs
+++ hardware_simulator/src/parser/mod.rs
@@ -1,1 +1,55 @@
+use lazy_static::lazy_static;
+use thiserror::Error;
+use std::sync::{Arc, RwLock};
+use std::collections::HashMap;
+
 mod instruction;
+
+lazy_static! {
+    static ref CHIP_TABLE: Arc<RwLock<HashMap<String, Chip>>> =
+        Arc::new(RwLock::new(HashMap::new()));
+}
+
+struct Chip {
+    in_pins: Vec<String>,
+    out_pins: Vec<String>,
+}
+
+#[derive(Eq, PartialEq, Debug)]
+struct Instruction<'a> {
+    chip_name: Symbol<'a>,
+    inputs: Vec<Argument<'a>>,
+}
+
+#[derive(Eq, PartialEq, Debug)]
+struct Argument<'a> {
+    internal: Symbol<'a>,
+    internal_bus: Option<BusRange>,
+    external: Symbol<'a>,
+    external_bus: Option<BusRange>,
+}
+
+#[derive(Eq, PartialEq, Debug)]
+enum Value {
+    True,
+    False,
+}
+
+#[derive(Eq, PartialEq, Debug)]
+enum Symbol<'a> {
+    Name(&'a str),
+    Value(Value),
+    Number(usize),
+}
+
+#[derive(Debug, Eq, PartialEq)]
+struct BusRange {
+    start: u16,
+    end: u16,
+}
+
+#[derive(Error, Debug, PartialEq)]
+pub enum HdlParseError<'a> {
+    #[error("Symbol `{0}` is not a valid symbol")]
+    BadSymbol(&'a str),
+}
