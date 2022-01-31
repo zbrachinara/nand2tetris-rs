eedc2ada7035c601e64f716194ff0bed4c441581
Eric Yu <zbrachinara@gmail.com>
2022-01-30 22:13:44 -0500

    implement native implementation parser

--- hardware_simulator/src/parser/chip.rs
+++ hardware_simulator/src/parser/chip.rs
@@ -1,8 +1,9 @@
+use crate::parser::connection::connection;
 use crate::parser::{name, spaced, Builtin, Connection, Implementation, PResult, Span};
 use nom::character::complete::char;
 use nom::combinator::opt;
-use nom::multi::separated_list0;
-use nom::sequence::{delimited, tuple};
+use nom::multi::{many1, separated_list0, separated_list1};
+use nom::sequence::{delimited, preceded, tuple};
 use nom_supreme::tag::complete::tag;
 
 fn builtin(arg: Span) -> PResult<Builtin> {
@@ -18,8 +19,8 @@ fn builtin(arg: Span) -> PResult<Builtin> {
     Ok((remainder, Builtin { name, clocked }))
 }
 
-fn native(_: Span) -> PResult<Vec<Connection>> {
-    todo!()
+fn native(arg: Span) -> PResult<Vec<Connection>> {
+    spaced(preceded(tag("PARTS:"), many1(connection)))(arg)
 }
 
 fn implementation(_: Span) -> PResult<Implementation> {
@@ -29,12 +30,13 @@ fn implementation(_: Span) -> PResult<Implementation> {
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::parser::{Argument, Symbol};
 
     #[test]
     fn test_builtin() {
         {
             let res = builtin(Span::new("BUILTIN DFF;\n     CLOCKED in;}")).unwrap();
-            let (remainder, Builtin {name, clocked}) = res;
+            let (remainder, Builtin { name, clocked }) = res;
             assert_eq!(*remainder, "}");
             assert_eq!(*name, "DFF");
 
@@ -45,7 +47,7 @@ mod test {
         }
         {
             let res = builtin(Span::new("BUILTIN DFF;\n     CLOCKED in, out;")).unwrap();
-            let (remainder, Builtin {name, clocked}) = res;
+            let (remainder, Builtin { name, clocked }) = res;
             assert_eq!(*remainder, "");
             assert_eq!(*name, "DFF");
 
@@ -57,11 +59,258 @@ mod test {
         }
         {
             let res = builtin(Span::new("BUILTIN DFF;\n     // CLOCKED in;\n}")).unwrap();
-            let (remainder, Builtin {name, clocked}) = res;
+            let (remainder, Builtin { name, clocked }) = res;
             assert_eq!(*remainder, "}");
             assert_eq!(*name, "DFF");
 
             assert!(matches!(clocked, None));
+        }
+    }
+
+    #[test]
+    fn test_native() {
+        {
+            let data = "\
+    PARTS:
+    Xor(a=a, b=b, out=neq1);
+    Xor(a=b, b=c, out=neq2);
+    Or (a=neq1, b=neq2, out=outOr);
+    Not(in=outOr, out=out);
+}";
+
+            let (remainder, connections) = native(Span::new(data)).unwrap();
+
+            assert_eq!(*remainder, "}");
+            assert_eq!(connections.len(), 4);
+
+            let checks = [
+                // checking Xor(a=a, b=b, out=neq1);
+                |conn: Connection| {
+                    let Connection { chip_name, inputs } = conn;
+                    assert_eq!(*chip_name, "Xor");
+                    assert_eq!(inputs.len(), 3);
+
+                    let checks = [
+                        |arg: Argument| {
+                            let Argument {
+                                internal,
+                                internal_bus,
+                                external,
+                                external_bus,
+                            } = arg;
+                            assert_eq!(*internal, "a");
+                            assert!(matches!(internal_bus, None));
+                            assert!(matches!(external, Symbol::Name(_)));
+                            if let Symbol::Name(external) = external {
+                                assert_eq!(*external, "a")
+                            }
+                            assert!(matches!(external_bus, None));
+                        },
+                        |arg: Argument| {
+                            let Argument {
+                                internal,
+                                internal_bus,
+                                external,
+                                external_bus,
+                            } = arg;
+                            assert_eq!(*internal, "b");
+                            assert!(matches!(internal_bus, None));
+                            assert!(matches!(external, Symbol::Name(_)));
+                            if let Symbol::Name(external) = external {
+                                assert_eq!(*external, "b")
+                            }
+                            assert!(matches!(external_bus, None))
+                        },
+                        |arg: Argument| {
+                            let Argument {
+                                internal,
+                                internal_bus,
+                                external,
+                                external_bus,
+                            } = arg;
+                            assert_eq!(*internal, "out");
+                            assert!(matches!(internal_bus, None));
+                            assert!(matches!(external, Symbol::Name(_)));
+                            if let Symbol::Name(external) = external {
+                                assert_eq!(*external, "neq1")
+                            }
+                            assert!(matches!(external_bus, None))
+                        },
+                    ];
+
+                    checks
+                        .into_iter()
+                        .zip(inputs.into_iter())
+                        .for_each(|(check, input)| check(input));
+                },
+                // checking Xor(a=b, b=c, out=neq2);
+                |conn: Connection| {
+                    let Connection { chip_name, inputs } = conn;
+                    assert_eq!(*chip_name, "Xor");
+                    assert_eq!(inputs.len(), 3);
+
+                    let checks = [
+                        |arg: Argument| {
+                            let Argument {
+                                internal,
+                                internal_bus,
+                                external,
+                                external_bus,
+                            } = arg;
+                            assert_eq!(*internal, "a");
+                            assert!(matches!(internal_bus, None));
+                            assert!(matches!(external, Symbol::Name(_)));
+                            if let Symbol::Name(external) = external {
+                                assert_eq!(*external, "b")
+                            }
+                            assert!(matches!(external_bus, None));
+                        },
+                        |arg: Argument| {
+                            let Argument {
+                                internal,
+                                internal_bus,
+                                external,
+                                external_bus,
+                            } = arg;
+                            assert_eq!(*internal, "b");
+                            assert!(matches!(internal_bus, None));
+                            assert!(matches!(external, Symbol::Name(_)));
+                            if let Symbol::Name(external) = external {
+                                assert_eq!(*external, "c")
+                            }
+                            assert!(matches!(external_bus, None))
+                        },
+                        |arg: Argument| {
+                            let Argument {
+                                internal,
+                                internal_bus,
+                                external,
+                                external_bus,
+                            } = arg;
+                            assert_eq!(*internal, "out");
+                            assert!(matches!(internal_bus, None));
+                            assert!(matches!(external, Symbol::Name(_)));
+                            if let Symbol::Name(external) = external {
+                                assert_eq!(*external, "neq2")
+                            }
+                            assert!(matches!(external_bus, None))
+                        },
+                    ];
+
+                    checks
+                        .into_iter()
+                        .zip(inputs.into_iter())
+                        .for_each(|(check, input)| check(input));
+                },
+                // checking Or(a=neq1, b=neq2, out=outOr);
+                |conn: Connection| {
+                    let Connection { chip_name, inputs } = conn;
+                    assert_eq!(*chip_name, "Or");
+                    assert_eq!(inputs.len(), 3);
+
+                    let checks = [
+                        |arg: Argument| {
+                            let Argument {
+                                internal,
+                                internal_bus,
+                                external,
+                                external_bus,
+                            } = arg;
+                            assert_eq!(*internal, "a");
+                            assert!(matches!(internal_bus, None));
+                            assert!(matches!(external, Symbol::Name(_)));
+                            if let Symbol::Name(external) = external {
+                                assert_eq!(*external, "neq1")
+                            }
+                            assert!(matches!(external_bus, None));
+                        },
+                        |arg: Argument| {
+                            let Argument {
+                                internal,
+                                internal_bus,
+                                external,
+                                external_bus,
+                            } = arg;
+                            assert_eq!(*internal, "b");
+                            assert!(matches!(internal_bus, None));
+                            assert!(matches!(external, Symbol::Name(_)));
+                            if let Symbol::Name(external) = external {
+                                assert_eq!(*external, "neq2")
+                            }
+                            assert!(matches!(external_bus, None))
+                        },
+                        |arg: Argument| {
+                            let Argument {
+                                internal,
+                                internal_bus,
+                                external,
+                                external_bus,
+                            } = arg;
+                            assert_eq!(*internal, "out");
+                            assert!(matches!(internal_bus, None));
+                            assert!(matches!(external, Symbol::Name(_)));
+                            if let Symbol::Name(external) = external {
+                                assert_eq!(*external, "outOr")
+                            }
+                            assert!(matches!(external_bus, None))
+                        },
+                    ];
+
+                    checks
+                        .into_iter()
+                        .zip(inputs.into_iter())
+                        .for_each(|(check, input)| check(input));
+                },
+                // checking Not(in=outOr, out=out);
+                |conn: Connection| {
+                    let Connection { chip_name, inputs } = conn;
+                    assert_eq!(*chip_name, "Not");
+                    assert_eq!(inputs.len(), 2);
+
+                    let checks = [
+                        |arg: Argument| {
+                            let Argument {
+                                internal,
+                                internal_bus,
+                                external,
+                                external_bus,
+                            } = arg;
+                            assert_eq!(*internal, "in");
+                            assert!(matches!(internal_bus, None));
+                            assert!(matches!(external, Symbol::Name(_)));
+                            if let Symbol::Name(external) = external {
+                                assert_eq!(*external, "outOr")
+                            }
+                            assert!(matches!(external_bus, None));
+                        },
+                        |arg: Argument| {
+                            let Argument {
+                                internal,
+                                internal_bus,
+                                external,
+                                external_bus,
+                            } = arg;
+                            assert_eq!(*internal, "out");
+                            assert!(matches!(internal_bus, None));
+                            assert!(matches!(external, Symbol::Name(_)));
+                            if let Symbol::Name(external) = external {
+                                assert_eq!(*external, "out")
+                            }
+                            assert!(matches!(external_bus, None))
+                        },
+                    ];
+
+                    checks
+                        .into_iter()
+                        .zip(inputs.into_iter())
+                        .for_each(|(check, input)| check(input));
+                },
+            ];
+
+            checks
+                .into_iter()
+                .zip(connections.into_iter())
+                .for_each(|(check, connection)| check(connection));
         }
     }
 }
