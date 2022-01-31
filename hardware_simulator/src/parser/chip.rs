3340bd7709285533a0f00cf44930e562292ea95c
Eric Yu <zbrachinara@gmail.com>
2022-01-30 21:34:00 -0500

    add parser for builtin implementation

--- hardware_simulator/src/parser/chip.rs
+++ hardware_simulator/src/parser/chip.rs
@@ -1,8 +1,21 @@
-use crate::parser::{Builtin, Connection, Implementation, PResult, Span};
+use crate::parser::{name, spaced, Builtin, Connection, Implementation, PResult, Span};
+use nom::character::complete::char;
+use nom::combinator::opt;
+use nom::multi::separated_list0;
+use nom::sequence::{delimited, tuple};
+use nom_supreme::tag::complete::tag;
 
-fn builtin(_: Span) -> PResult<Builtin> {
+fn builtin(arg: Span) -> PResult<Builtin> {
+    let (remainder, (name, clocked)) = tuple((
+        spaced(delimited(tag("BUILTIN"), name, char(';'))),
+        opt(spaced(delimited(
+            tag("CLOCKED"),
+            separated_list0(char(','), name),
+            char(';'),
+        ))),
+    ))(arg)?;
 
-    todo!()
+    Ok((remainder, Builtin { name, clocked }))
 }
 
 fn native(_: Span) -> PResult<Vec<Connection>> {
@@ -11,4 +24,44 @@ fn native(_: Span) -> PResult<Vec<Connection>> {
 
 fn implementation(_: Span) -> PResult<Implementation> {
     todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_builtin() {
+        {
+            let res = builtin(Span::new("BUILTIN DFF;\n     CLOCKED in;")).unwrap();
+            let (remainder, Builtin {name, clocked}) = res;
+            assert_eq!(*remainder, "");
+            assert_eq!(*name, "DFF");
+
+            assert!(matches!(clocked, Some(_)));
+            if let Some(clocked) = clocked {
+                assert_eq!(*(clocked[0]), "in");
+            }
+        }
+        {
+            let res = builtin(Span::new("BUILTIN DFF;\n     CLOCKED in, out;")).unwrap();
+            let (remainder, Builtin {name, clocked}) = res;
+            assert_eq!(*remainder, "");
+            assert_eq!(*name, "DFF");
+
+            assert!(matches!(clocked, Some(_)));
+            if let Some(clocked) = clocked {
+                assert_eq!(*(clocked[0]), "in");
+                assert_eq!(*(clocked[1]), "out");
+            }
+        }
+        {
+            let res = builtin(Span::new("BUILTIN DFF;\n     // CLOCKED in;")).unwrap();
+            let (remainder, Builtin {name, clocked}) = res;
+            assert_eq!(*remainder, "");
+            assert_eq!(*name, "DFF");
+
+            assert!(matches!(clocked, None));
+        }
+    }
 }
