e69f4755b89ddc1e73f42afe31e63949be7dc997
Eric Yu <zbrachinara@gmail.com>
2022-02-04 19:18:18 -0500

    added dedicated struct in place of tuple

--- hardware_simulator/src/model/native/build.rs
+++ hardware_simulator/src/model/native/build.rs
@@ -1,25 +1,32 @@
-use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 use crate::bus_range::BusRange;
+use crate::model::parser::{Argument, Connection as ConnRepr, Symbol};
 use crate::model::Chip;
-use crate::model::parser::{Argument, Connection, Symbol};
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Connection {
+    index: usize,
+    range: BusRange,
+}
 
 pub fn connections_by_pin(
-    connections: &Vec<Connection>,
+    connections: &Vec<ConnRepr>,
     dependents: &Vec<Box<dyn Chip>>,
-) -> HashMap<String, Vec<(usize, BusRange)>> {
-    let mut pin_map = HashMap::new();
+) -> HashMap<String, Vec<Connection>> {
+    let mut pin_map: HashMap<_, Vec<_>> = HashMap::new();
 
-    let mut insert = |k: String, v: (usize, BusRange)| {
-        pin_map
-            .entry(k)
-            .and_modify(|e: &mut Vec<_>| e.push(v.clone()))
-            .or_insert(vec![v]);
+    let mut insert = |k: String, v: Connection| {
+        match pin_map.entry(k) {
+            Entry::Occupied(mut e) => {e.get_mut().push(v);}
+            Entry::Vacant(e) => {e.insert(vec![v]);}
+        }
     };
 
     connections
         .iter()
         .enumerate()
-        .for_each(|(index, Connection { inputs, .. })| {
+        .for_each(|(index, ConnRepr { inputs, .. })| {
             let interface = dependents[index].interface();
             let _ = inputs.iter().try_for_each::<_, Result<(), ()>>(
                 |Argument {
@@ -30,12 +37,13 @@ pub fn connections_by_pin(
                  }| {
                     // TODO: Handle output pin indexing
                     match external {
-                        Symbol::Name(external) => {
-                            insert(
-                                external.to_string(),
-                                (index, interface.real_range(internal, internal_bus.clone())?),
-                            )
-                        }
+                        Symbol::Name(external) => insert(
+                            external.to_string(),
+                            Connection {
+                                index,
+                                range: interface.real_range(internal, internal_bus.clone())?,
+                            },
+                        ),
                         Symbol::Value(v) => {
                             todo!()
                         }
