7d0e62a9b5e4f78fcca09c53b963e5902da9e558
Eric Yu <zbrachinara@gmail.com>
2022-02-09 09:12:44 -0500

    remove function `edges_from_connections`

--- hardware_simulator/src/model/chip/native/build.rs
+++ hardware_simulator/src/model/chip/native/build.rs
@@ -55,110 +55,110 @@ pub enum ClockBehavior {
     Sequential,
 }
 
-fn edges_from_connections(
-    conn_names: &Vec<parser::Connection>,
-    dependents: &Vec<Box<dyn Chip>>,
-) -> HashMap<String, EdgeSet> {
-    let mut pin_map: HashMap<_, EdgeSet> = HashMap::new();
-
-    let mut insert = |k: String, v: Endpoint, as_input: bool| {
-        match pin_map.entry(k) {
-            Entry::Occupied(mut e) => {
-                e.get_mut().add(v, as_input)
-                // Ok(())
-            }
-            Entry::Vacant(e) => {
-                e.insert(EdgeSet::new_with(v, as_input)?);
-                Ok(())
-            }
-        }
-    };
-
-    let input_interface = dependents[conn_names.len()].interface();
-    let output_interface = dependents[conn_names.len() + 1].interface();
-
-    for (index, parser::Connection { inputs, .. }) in conn_names.iter().enumerate() {
-        let interface = dependents[index].interface();
-        let res = inputs.iter().try_for_each::<_, Result<(), ()>>(|argument| {
-            let Argument {
-                internal,
-                internal_bus,
-                external,
-                external_bus,
-            } = argument;
-            match external {
-                Symbol::Name(external) => {
-                    let (external, raw) = {
-                        let r = **external;
-                        let external = if let Some(bus) = external_bus {
-                            format!("{external}.{}.{}", bus.start, bus.end)
-                        } else {
-                            external.to_string()
-                        };
-                        (external, r)
-                    };
-
-                    if let Ok(bus) = input_interface.real_range(&raw, external_bus.clone()) {
-                        println!("Inserting input connection on {}", conn_names.len());
-                        insert(
-                            external.to_string(),
-                            Endpoint {
-                                index: conn_names.len(),
-                                range: bus,
-                                com_or_seq: ClockBehavior::Combinatorial,
-                            },
-                            true, // it's an output of the input bus
-                        );
-                    } else if let Ok(bus) = output_interface.real_range(&raw, external_bus.clone())
-                    {
-                        println!("Inserting output connection on {}", conn_names.len() + 1);
-                        insert(
-                            external.to_string(),
-                            Endpoint {
-                                index: conn_names.len() + 1,
-                                range: bus,
-                                com_or_seq: ClockBehavior::Combinatorial,
-                            },
-                            false, // it's an input of the output bus
-                        );
-                    }
-
-                    println!(
-                        "Pin {internal} is {}",
-                        if interface.is_input(&internal) {
-                            "input"
-                        } else {
-                            "output"
-                        }
-                    );
-                    insert(
-                        external.to_string(),
-                        Endpoint {
-                            index,
-                            range: interface.real_range(internal, internal_bus.clone())?,
-                            com_or_seq: ClockBehavior::Combinatorial,
-                        },
-                        !interface.is_input(&internal),
-                    )?;
-                }
-                Symbol::Value(_) => {
-                    todo!()
-                }
-                Symbol::Number(_) => {
-                    todo!()
-                }
-            };
-
-            Ok(())
-        });
-
-        if let Err(x) = res {
-            println!("error: {x:?}");
-        }
-    }
-
-    pin_map
-}
+// fn edges_from_connections(
+//     conn_names: &Vec<parser::Connection>,
+//     dependents: &Vec<Box<dyn Chip>>,
+// ) -> HashMap<String, EdgeSet> {
+//     let mut pin_map: HashMap<_, EdgeSet> = HashMap::new();
+//
+//     let mut insert = |k: String, v: Endpoint, as_input: bool| {
+//         match pin_map.entry(k) {
+//             Entry::Occupied(mut e) => {
+//                 e.get_mut().add(v, as_input)
+//                 // Ok(())
+//             }
+//             Entry::Vacant(e) => {
+//                 e.insert(EdgeSet::new_with(v, as_input)?);
+//                 Ok(())
+//             }
+//         }
+//     };
+//
+//     let input_interface = dependents[conn_names.len()].interface();
+//     let output_interface = dependents[conn_names.len() + 1].interface();
+//
+//     for (index, parser::Connection { inputs, .. }) in conn_names.iter().enumerate() {
+//         let interface = dependents[index].interface();
+//         let res = inputs.iter().try_for_each::<_, Result<(), ()>>(|argument| {
+//             let Argument {
+//                 internal,
+//                 internal_bus,
+//                 external,
+//                 external_bus,
+//             } = argument;
+//             match external {
+//                 Symbol::Name(external) => {
+//                     let (external, raw) = {
+//                         let r = **external;
+//                         let external = if let Some(bus) = external_bus {
+//                             format!("{external}.{}.{}", bus.start, bus.end)
+//                         } else {
+//                             external.to_string()
+//                         };
+//                         (external, r)
+//                     };
+//
+//                     if let Ok(bus) = input_interface.real_range(&raw, external_bus.clone()) {
+//                         println!("Inserting input connection on {}", conn_names.len());
+//                         insert(
+//                             external.to_string(),
+//                             Endpoint {
+//                                 index: conn_names.len(),
+//                                 range: bus,
+//                                 com_or_seq: ClockBehavior::Combinatorial,
+//                             },
+//                             true, // it's an output of the input bus
+//                         );
+//                     } else if let Ok(bus) = output_interface.real_range(&raw, external_bus.clone())
+//                     {
+//                         println!("Inserting output connection on {}", conn_names.len() + 1);
+//                         insert(
+//                             external.to_string(),
+//                             Endpoint {
+//                                 index: conn_names.len() + 1,
+//                                 range: bus,
+//                                 com_or_seq: ClockBehavior::Combinatorial,
+//                             },
+//                             false, // it's an input of the output bus
+//                         );
+//                     }
+//
+//                     println!(
+//                         "Pin {internal} is {}",
+//                         if interface.is_input(&internal) {
+//                             "input"
+//                         } else {
+//                             "output"
+//                         }
+//                     );
+//                     insert(
+//                         external.to_string(),
+//                         Endpoint {
+//                             index,
+//                             range: interface.real_range(internal, internal_bus.clone())?,
+//                             com_or_seq: ClockBehavior::Combinatorial,
+//                         },
+//                         !interface.is_input(&internal),
+//                     )?;
+//                 }
+//                 Symbol::Value(_) => {
+//                     todo!()
+//                 }
+//                 Symbol::Number(_) => {
+//                     todo!()
+//                 }
+//             };
+//
+//             Ok(())
+//         });
+//
+//         if let Err(x) = res {
+//             println!("error: {x:?}");
+//         }
+//     }
+//
+//     pin_map
+// }
 
 struct Dependency<'a> {
     index: NodeIndex<u32>,
