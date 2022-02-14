28261380d2493ac5f4e1f46de2a324a5687d9840
Eric Yu <zbrachinara@gmail.com>
2022-02-14 08:17:47 -0500

    add test

---
+++ hardware_simulator/tests/not.rs
@@ -0,0 +1,15 @@
+use hardware_simulator::model::chip::build_ctx::ChipBuilder;
+
+#[test]
+fn load_step_not() {
+
+    let hdl_dir = std::env::current_dir().unwrap().join("../test_files");
+    let not_file = hdl_dir.join("Not.hdl");
+
+    let mut builder = ChipBuilder::new();
+    builder.add_hdl(not_file);
+    let mut chip = builder.resolve_chip("Not").unwrap();
+
+    chip.eval(&[false]);
+
+}
