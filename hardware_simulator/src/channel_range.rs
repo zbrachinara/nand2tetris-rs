63d1fbf4f094d7cacd104459796cf2d63420fa79
Eric Yu <zbrachinara@gmail.com>
2022-02-18 09:18:18 -0500

    add conversion from `ChannelRange` into std `Range`

--- hardware_simulator/src/channel_range.rs
+++ hardware_simulator/src/channel_range.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct ChannelRange {
     pub start: u16,
@@ -7,5 +9,14 @@ pub struct ChannelRange {
 impl ChannelRange {
     pub fn size(&self) -> u16 {
         self.end - self.start
+    }
+    pub fn as_range(&self) -> Range<usize> {
+        self.clone().into()
+    }
+}
+
+impl From<ChannelRange> for Range<usize> {
+    fn from(r: ChannelRange) -> Self {
+        (r.start as usize)..(r.end as usize)
     }
 }
