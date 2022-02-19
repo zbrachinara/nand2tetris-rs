1812b8027611e59f08c6832620a3643db16e9ba9
Eric Yu <zbrachinara@gmail.com>
2022-02-18 19:41:57 -0500

    affirm quality of running `Not` chip

--- hardware_simulator/tests/not.rs
+++ hardware_simulator/tests/not.rs
@@ -10,6 +10,7 @@ fn load_step_not() {
     builder.add_hdl(not_file);
     let mut chip = builder.resolve_chip("Not").unwrap();
 
-    chip.eval(&[false]);
+    assert_eq!([true].as_slice(), chip.eval(&[false]));
+    assert_eq!([false].as_slice(), chip.eval(&[true]));
 
 }
