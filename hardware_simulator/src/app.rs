a2a6964062a7269138cad8ae624d122e62307446
Eric Yu <zbrachinara@gmail.com>
2022-02-22 12:25:24 -0500

    put in panels on left and right

--- hardware_simulator/src/app.rs
+++ hardware_simulator/src/app.rs
@@ -1,6 +1,5 @@
-use std::time::Duration;
-use eframe::egui::{CtxRef, Rgba, Vec2};
-use eframe::epi::{Frame, Storage};
+use eframe::egui::{self, Align, CtxRef, Layout, TextEdit, Ui, Vec2};
+use eframe::epi::Frame;
 
 pub struct App {
     size: Vec2,
@@ -8,16 +7,31 @@ pub struct App {
 
 impl App {
     pub fn new(width: u32, height: u32) -> Self {
-        crate::log("Starting to make the app");
         Self {
-            size: Vec2::new(width as f32, height as f32)
+            size: Vec2::new(width as f32, height as f32),
         }
     }
 }
 
 impl eframe::epi::App for App {
-    fn update(&mut self, ctx: &CtxRef, frame: &Frame) {
-        crate::log("Updating correctly");
+    fn update(&mut self, ctx: &CtxRef, _: &Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let size = ui.available_size();
+            let width = size.x / 2f32;
+            let height = size.y;
+            let elem_size = Vec2::new(width, height);
+            ui.columns(2, |uis| {
+                uis[0].add_sized(elem_size, TextEdit::multiline(&mut "when the"));
+                uis[1].allocate_ui_with_layout(
+                    elem_size,
+                    Layout::top_down_justified(Align::Center),
+                    |ui| {
+                        ui.heading("Pins");
+                        self.pin_table(ui);
+                    },
+                );
+            });
+        });
     }
 
     fn name(&self) -> &str {
@@ -26,5 +40,22 @@ impl eframe::epi::App for App {
 
     fn max_size_points(&self) -> Vec2 {
         self.size.clone()
+    }
+}
+
+impl App {
+    fn pin_table(&self, ui: &mut Ui) {
+        ui.with_layout(
+            egui::Layout::centered_and_justified(egui::Direction::TopDown),
+            |ui| {
+                egui::Grid::new("_external_pin_table")
+                    .num_columns(2)
+                    .show(ui, |ui| {
+                        ui.label("Pin Name");
+                        ui.label("Pin Value");
+                        ui.end_row();
+                    });
+            },
+        );
     }
 }
