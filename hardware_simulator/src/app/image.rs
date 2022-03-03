f33c65abf172dc0da215adb52b5c751c2d952293
Eric Yu <zbrachinara@gmail.com>
2022-03-02 21:51:52 -0500

    add feature for controlling whether resources are compiled into binary

--- hardware_simulator/src/app/image.rs
+++ hardware_simulator/src/app/image.rs
@@ -1,11 +1,15 @@
 use eframe::egui::ColorImage;
+#[cfg(feature = "compile_resources")]
 use include_dir::{include_dir, Dir};
+use itertools::Itertools;
 use once_cell::sync::Lazy;
 use std::borrow::Cow;
-use itertools::Itertools;
+use std::fs;
 
-pub static IMAGE_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/images");
+#[cfg(feature = "compile_resources")]
+static IMAGE_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/images");
 
+#[cfg(feature = "compile_resources")]
 pub static IMAGE_DATA: Lazy<Vec<(Cow<str>, ColorImage)>> = Lazy::new(|| {
     IMAGE_DIR
         .files()
@@ -17,6 +21,35 @@ pub static IMAGE_DATA: Lazy<Vec<(Cow<str>, ColorImage)>> = Lazy::new(|| {
                 image.to_rgba8().as_flat_samples().as_slice(),
             );
             (name, color_image)
+        })
+        .collect_vec()
+});
+
+#[cfg(not(feature = "compile_resources"))]
+pub static IMAGE_DATA: Lazy<Vec<(Cow<str>, ColorImage)>> = Lazy::new(|| {
+    //
+    let path = std::env::current_dir()
+        .unwrap()
+        .join("hardware_simulator/images");
+    fs::read_dir(path)
+        .unwrap()
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+            if entry.file_type().unwrap().is_file() {
+                Some(entry.path())
+            } else {
+                None
+            }
+        })
+        .map(|path| {
+            let name = path.file_stem().unwrap().to_string_lossy().to_string();
+            let image =
+                image::load_from_memory(fs::read(path.clone()).unwrap().as_slice()).unwrap();
+            let color_image = ColorImage::from_rgba_unmultiplied(
+                [image.width() as _, image.height() as _],
+                image.to_rgba8().as_flat_samples().as_slice(),
+            );
+            (Cow::Owned(name), color_image)
         })
         .collect_vec()
 });
