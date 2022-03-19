4542c76a0135488834ae1259e617570814124cc2
Eric Yu <zbrachinara@gmail.com>
2022-03-19 08:18:57 -0400

    use structopt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 #![allow(dead_code)]
 
+use structopt::*;
+
 mod parse;
 mod assemble;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "nand2tetris-assembler",
+    about = "assembles assembly written for the nand2tetris vm into hardware language"
+)]
+struct Opt {
+
+}
 
 fn main() {
     println!("Hello, world!");
