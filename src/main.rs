cd0844c339ca4fd1671826ce6ce1aa6a7c166d90
Eric Yu <zbrachinara@gmail.com>
2022-03-19 08:35:32 -0400

    get file name from arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![allow(dead_code)]
 
+use std::fs;
 use structopt::*;
 
-mod parse;
 mod assemble;
+mod parse;
 
 #[derive(Debug, StructOpt)]
 #[structopt(
@@ -11,9 +12,13 @@ mod assemble;
     about = "assembles assembly written for the nand2tetris vm into hardware language"
 )]
 struct Opt {
-
+    #[structopt(name = "FILE")]
+    file_name: String,
 }
 
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    let file = fs::read_to_string(opt.file_name);
+
+
 }
