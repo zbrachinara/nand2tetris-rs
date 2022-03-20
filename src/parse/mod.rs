5cd03ec9367be56c56aa1762af6cf2324b904003
Eric Yu <zbrachinara@gmail.com>
2022-03-20 12:25:27 -0400

    rewrite publicized program parser

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -5,7 +5,10 @@ mod structs;
 
 use nom::IResult;
 pub use structs::*;
-pub use parse::program;
+
+pub fn program(program: &str) -> Result<Program, nom::Err<nom::error::Error<&str>>> {
+    parse::program(program).map(|(_, program)| program)
+}
 
 type PResult<'a, T> = IResult<&'a str, T>;
 
