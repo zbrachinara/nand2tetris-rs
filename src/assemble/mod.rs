0c7c9f81a1850edb4c257f12066615a75413a610
Eric Yu <zbrachinara@gmail.com>
2022-03-20 10:48:24 -0400

    correctly populate labeled ROM addresses

--- src/assemble/mod.rs
+++ src/assemble/mod.rs
@@ -1,24 +1,31 @@
 mod predefined;
 
-use crate::assemble::predefined::SymbolTable;
+use crate::assemble::predefined::{Address, SymbolTable};
 use crate::parse::{Instruction, Program};
 
 pub fn assemble_program(program: Program) -> Vec<u16> {
     let output = vec![];
+    let mut symbol_table = SymbolTable::new();
+
+    // populate symbol table with rom addresses
+    {
+        let mut instr_count = 0;
+        for instr in program {
+            match instr {
+                Instruction::Label(str) => {
+                    symbol_table.insert(str.clone(), Address::Rom(instr_count))
+                }
+                _ => instr_count += 1,
+            }
+        }
+    }
 
     let Program(program) = program;
-    let (labels, instructions) = program
+    let (_, instructions) = program
         .into_iter()
         .partition::<Vec<_>, _>(|instr| matches!(instr, Instruction::Label(_)));
 
-    let mut symbol_table = SymbolTable::new();
-    for label in labels {
-        symbol_table.assign_available_ram(label.label());
-    }
-
-    for instr in instructions {
-
-    }
+    for instr in instructions {}
 
     output
 }
