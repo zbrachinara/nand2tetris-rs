48d93ffce1e265249fa2bd5a7af71f052a051722
Eric Yu <zbrachinara@gmail.com>
2022-03-20 14:43:11 -0400

    fill unused spaces with 1 for consistency with standard assembler

--- src/assemble/convert.rs
+++ src/assemble/convert.rs
@@ -1,5 +1,5 @@
 use crate::parse::{CExpr, Dst, Ident, JumpCondition};
 
 pub const fn cinstr(expr: &CExpr, dst: &Dst, jmp: &JumpCondition) -> u16 {
-    0b1000_0000_0000_0000 | expr.as_bits() | dst.as_bits() | jmp.as_bits()
+    0b1110_0000_0000_0000 | expr.as_bits() | dst.as_bits() | jmp.as_bits()
 }
