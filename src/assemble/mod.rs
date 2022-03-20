c5b1599e175c2f4bce7e6fcc201a9ad9499e4292
Eric Yu <zbrachinara@gmail.com>
2022-03-20 08:15:29 -0400

    set up basic structure for assembling

--- src/assemble/mod.rs
+++ src/assemble/mod.rs
@@ -1,2 +1,22 @@
+mod predefined;
+
+use crate::assemble::predefined::new_symbol_table;
+use crate::parse::{Instruction, Program};
+
+pub fn assemble_program(program: Program) -> Vec<u16> {
+    let output = vec![];
+
+    let Program(program) = program;
+    let (labels, instructions) = program
+        .into_iter()
+        .partition::<Vec<_>, _>(|instr| matches!(instr, Instruction::Label(_)));
+
+    let symbol_table = new_symbol_table();
+
+    for instr in instructions {
+
+    }
 
 
+    output
+}
---
+++ src/assemble/predefined.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+pub const SYMBOLS: &[(&'static str, u16)] = &[
+    ("R0", 0),
+    ("R1", 1),
+    ("R2", 2),
+    ("R3", 3),
+    ("R4", 4),
+    ("R5", 5),
+    ("R6", 6),
+    ("R7", 7),
+    ("R8", 8),
+    ("R9", 9),
+    ("R10", 10),
+    ("R11", 11),
+    ("R12", 12),
+    ("R13", 13),
+    ("R14", 14),
+    ("R15", 15),
+    ("SP", 0),
+    ("LCL", 1),
+    ("ARG", 2),
+    ("THIS", 3),
+    ("THAT", 4),
+    ("SCREEN", 0x4000),
+    ("KBD", 0x6000),
+];
+
+pub fn new_symbol_table() -> HashMap<String, u16> {
+    SYMBOLS
+        .into_iter()
+        .map(|(s, x)| (s.to_string(), x.clone()))
+        .collect()
+}
