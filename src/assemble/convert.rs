9e5e2bb68f84492fe04867472bbadd60b76a459d
Eric Yu <zbrachinara@gmail.com>
2022-03-21 14:22:08 -0400

    disable clippy pedantic warning on pass by ref

--- src/assemble/convert.rs
+++ src/assemble/convert.rs
@@ -1,5 +1,6 @@
 use crate::parse::{CExpr, Dst, JumpCondition};
 
+#[allow(clippy::trivially_copy_pass_by_ref)]
 pub const fn cinstr(expr: &CExpr, dst: &Dst, jmp: &JumpCondition) -> u16 {
     0b1110_0000_0000_0000 | expr.as_bits() | dst.as_bits() | jmp.as_bits()
 }
