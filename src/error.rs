5039cd10da4bc6ee4914dc65aa286017ad98ac21
Eric Yu <zbrachinara@gmail.com>
2022-03-21 08:35:43 -0400

    created conversion between nom errors and self-defined errors

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,17 @@
+use nom::Err;
 
 #[derive(Debug, thiserror::Error)]
 #[allow(clippy::module_name_repetitions)]
 pub enum AssemblyError {
+    #[error("The assembler had an internal problem -- please report")]
+    Internal,
+}
 
+impl From<nom::Err<AssemblyError>> for AssemblyError {
+    fn from(nom_error: Err<AssemblyError>) -> Self {
+        match nom_error {
+            Err::Incomplete(_) => AssemblyError::Internal,
+            Err::Error(e) | Err::Failure(e) => e,
+        }
+    }
 }
