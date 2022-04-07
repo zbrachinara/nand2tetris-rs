dafdc1e317ab4694e505943930d215227ed36401
Eric Yu <zbrachinara@gmail.com>
2022-04-07 06:00:58 -0400

    simplified program by removing flexstr

--- src/err/str.rs
+++ src/err/str.rs
@@ -1,27 +1,15 @@
-use flexstr::ToLocalStr;
-use flexstr::local_str;
-
 const MAX_CONTEXT_LEN: usize = 20;
 
 pub fn legible_string(s: &str) -> String {
-
-    let mut len = 0;
-
     let modified_s = s
         .chars()
-        .map(|c| match c {
-            '\n' => local_str!(","),
-            '\r' => local_str!(""),
-            x => x.to_local_str(),
+        .filter_map(|c| match c {
+            '\n' => Some(','),
+            '\r' => None,
+            x => Some(x),
         })
-        .take_while(|s| {
-            len += s.len();
-            len < MAX_CONTEXT_LEN
-        })
-        .fold(String::with_capacity(MAX_CONTEXT_LEN), |mut acc, c| {
-            acc.push_str(c.as_str());
-            acc
-        });
+        .take(MAX_CONTEXT_LEN)
+        .collect::<String>();
 
     if s.len() > MAX_CONTEXT_LEN {
         modified_s + "..."
