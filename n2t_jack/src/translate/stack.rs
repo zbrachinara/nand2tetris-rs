493002261596bae3eaa01d07282c439e796bcd88
Eric Yu <zbrachinara@gmail.com>
2022-04-22 14:15:44 -0400

    add translation method for stack operations

---
+++ n2t_jack/src/translate/stack.rs
@@ -0,0 +1,9 @@
+use n2t_asm::parse::Item;
+use crate::translate::keyword::Stack;
+
+impl Stack {
+    pub fn translate(&self, words: impl Iterator<Item = String>) -> Vec<Item> {
+
+        todo!()
+    }
+}
