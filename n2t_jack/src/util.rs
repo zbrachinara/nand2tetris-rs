ca67a914a6ce15d6d5a24501ca370ae2c41267af
Eric Yu <zbrachinara@gmail.com>
2022-04-22 07:51:48 -0400

    make `const_concat` easier to use

--- n2t_jack/src/util.rs
+++ n2t_jack/src/util.rs
@@ -1,4 +1,3 @@
-
 use std::mem::MaybeUninit;
 
 /// Concatenates two arrays at compile time
@@ -14,5 +13,22 @@ pub const fn const_concat<T, const N: usize, const M: usize>(
         std::ptr::copy_nonoverlapping(arr2.as_ptr(), combined.as_mut_ptr().add(N), M);
         combined
     }
+}
 
+#[macro_export]
+macro_rules! const_concat {
+    ($arr1:expr, $arr2:expr) => {
+        $crate::util::const_concat(&$arr1, &$arr2)
+    };
+    ($arr1:expr, $($arrays:expr),+) => {
+        {
+            let arrx = $crate::const_concat!($($arrays),+);
+            let res = $crate::util::const_concat(
+                &$arr1,
+                &arrx
+            );
+            core::mem::forget(arrx);
+            res
+        }
+    };
 }
