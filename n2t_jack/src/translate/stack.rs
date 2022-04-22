43ca4acf1cf9e63d50ab57aff9d54227f2dae9f4
Eric Yu <zbrachinara@gmail.com>
2022-04-22 15:12:16 -0400

    added parsing structure for segment names

--- n2t_jack/src/translate/stack.rs
+++ n2t_jack/src/translate/stack.rs
@@ -1,9 +1,23 @@
 use n2t_asm::parse::Item;
+use std::str::FromStr;
 use strum_macros::EnumString;
 
 impl Stack {
-    pub fn translate(&self, words: impl Iterator<Item = String>) -> Vec<Item> {
+    pub fn translate<'a>(&self, mut words: impl Iterator<Item = &'a str>) -> Result<Vec<Item>, ()> {
+        if let Some(Ok(segment)) = words.next().map(|seg_ident| Segment::from_str(seg_ident)) {
+            let offset = words
+                .next()
+                .map(|s| u16::from_str_radix(s, 10).map_err(|_| ()))
+                .unwrap_or(Err(()))?;
+            segment.translate(offset)
+        } else {
+            Err(())
+        }
+    }
+}
 
+impl Segment {
+    pub fn translate(&self, offset: u16) -> Result<Vec<Item>, ()> {
         todo!()
     }
 }
@@ -13,4 +27,17 @@ impl Stack {
 pub enum Stack {
     Push,
     Pop,
+}
+
+#[derive(EnumString)]
+#[strum(serialize_all = "lowercase")]
+pub enum Segment {
+    Local,
+    Argument,
+    This,
+    That,
+    Pointer,
+    Temp,
+    Static,
+    Constant,
 }
