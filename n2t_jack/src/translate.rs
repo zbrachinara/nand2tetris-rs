de320a64251d69cff58bddfb5e676f107f91ce67
Eric Yu <zbrachinara@gmail.com>
2022-04-21 21:04:01 -0400

    added initial translation logic

---
+++ n2t_jack/src/translate.rs
@@ -0,0 +1,18 @@
+use n2t_asm::parse::Item;
+
+fn translate(program: &str) -> impl Iterator<Item = Result<Item, ()>> {
+    program
+        .lines()
+        .filter_map(|line| {
+            line.split_once("//")
+                .map(|(line, _)| line.trim())
+                .and_then(|line| (!line.is_empty()).then(|| line))
+        })
+        .flat_map(|instr| translate_instruction(instr));
+
+    std::iter::empty()
+}
+
+fn translate_instruction(instruction: &str) -> impl Iterator<Item = Result<Item, ()>> {
+    std::iter::empty()
+}
