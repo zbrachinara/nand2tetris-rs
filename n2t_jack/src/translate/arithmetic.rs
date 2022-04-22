d5e2ad56e929e47c1169a1ac33fb3bfe1a4763ea
Eric Yu <zbrachinara@gmail.com>
2022-04-22 11:58:21 -0400

    implemented translations for conditions

--- n2t_jack/src/translate/arithmetic.rs
+++ n2t_jack/src/translate/arithmetic.rs
@@ -1,8 +1,10 @@
 use super::common::*;
 use crate::const_concat;
 use crate::translate::keyword::Arithmetic;
-use n2t_asm::parse::Source;
 use n2t_asm::parse::{CExpr, Dst, Instruction, Item, JumpCondition};
+use n2t_asm::parse::{Ident, Source};
+
+const HIGH_BIT: u16 = 0b1000_0000_0000_0000;
 
 const ADD: &[Item] = &const_concat!(
     STACK_CALL_ON_TWO,
@@ -58,15 +60,91 @@ const NOT: &[Item] = &const_concat!(
     })],
 );
 
+const EQ: &[Item] = &const_concat!(
+    STACK_CALL_ON_TWO,
+    [
+        Item::Instruction(Instruction::C {
+            expr: CExpr::XMinusD(Source::Memory),
+            dst: Dst::D,
+            jump: JumpCondition::Never,
+        }),
+        Item::Instruction(Instruction::C {
+            expr: CExpr::NotD,
+            dst: Dst::M,
+            jump: JumpCondition::Never,
+        })
+    ],
+);
+
+const LT: &[Item] = &const_concat!(
+    STACK_CALL_ON_TWO,
+    [
+        Item::Instruction(Instruction::C {
+            expr: CExpr::XMinusD(Source::Memory),
+            dst: Dst::D,
+            jump: JumpCondition::Never,
+        }),
+        Item::Instruction(Instruction::A(Ident::Addr(HIGH_BIT))),
+        Item::Instruction(Instruction::C {
+            expr: CExpr::DAndX(Source::Register),
+            dst: Dst::D,
+            jump: JumpCondition::Never,
+        }),
+    ],
+    FETCH_STACK_POINTER,
+    [
+        Item::Instruction(Instruction::C {
+            expr: CExpr::XMinusOne(Source::Memory),
+            dst: Dst::A,
+            jump: JumpCondition::Never,
+        }),
+        Item::Instruction(Instruction::C {
+            expr: CExpr::D,
+            dst: Dst::M,
+            jump: JumpCondition::Never,
+        })
+    ],
+);
+
+const GT: &[Item] = &const_concat!(
+    STACK_CALL_ON_TWO,
+    [
+        Item::Instruction(Instruction::C {
+            expr: CExpr::DMinusX(Source::Memory),
+            dst: Dst::D,
+            jump: JumpCondition::Never,
+        }),
+        Item::Instruction(Instruction::A(Ident::Addr(HIGH_BIT))),
+        Item::Instruction(Instruction::C {
+            expr: CExpr::DAndX(Source::Register),
+            dst: Dst::D,
+            jump: JumpCondition::Never,
+        }),
+    ],
+    FETCH_STACK_POINTER,
+    [
+        Item::Instruction(Instruction::C {
+            expr: CExpr::XMinusOne(Source::Memory),
+            dst: Dst::A,
+            jump: JumpCondition::Never,
+        }),
+        Item::Instruction(Instruction::C {
+            expr: CExpr::D,
+            dst: Dst::M,
+            jump: JumpCondition::Never,
+        })
+    ],
+);
+
 impl Arithmetic {
     pub fn translate(self) -> &'static [Item] {
         match self {
             Arithmetic::Add => ADD,
             Arithmetic::Sub => SUB,
             Arithmetic::Neg => NEG,
-            Arithmetic::Eq => todo!(),
-            Arithmetic::Gt => todo!(),
-            Arithmetic::Lt => todo!(),
+            Arithmetic::Eq => EQ, //TODO: Define determinant bit
+            Arithmetic::Gt => GT,
+            Arithmetic::Lt => LT,
             Arithmetic::And => AND,
             Arithmetic::Or => OR,
             Arithmetic::Not => NOT,
--- n2t_jack/src/translate/keyword.rs
+++ n2t_jack/src/translate/keyword.rs
@@ -6,8 +6,54 @@ pub enum Arithmetic {
     Add,
     Sub,
     Neg,
+    /// ```text
+    /// @0
+    /// M=M-1
+    /// A=M
+    /// D=M
+    /// A=A-1
+    ///
+    /// M=M-D
+    /// M=!M
+    /// ```
     Eq,
+    /// ```text
+    /// // stack pointer access
+    /// @0
+    /// M=M-1
+    /// A=M
+    /// D=M
+    /// A=A-1
+    ///
+    /// // write negative bit to D
+    /// D=D-M
+    /// @0b1000_0000_0000_0000
+    /// D=D&A
+    ///
+    /// // write D to correct stack location
+    /// @0
+    /// A=M-1
+    /// M=D
+    /// ```
     Gt,
+    /// ```text
+    /// // stack pointer access
+    /// @0
+    /// M=M-1
+    /// A=M
+    /// D=M
+    /// A=A-1
+    ///
+    /// // write negative bit to D
+    /// D=M-D
+    /// @0b1000_0000_0000_0000
+    /// D=D&A
+    ///
+    /// // write D to correct stack location
+    /// @0
+    /// A=M-1
+    /// M=D
+    /// ```
     Lt,
     And,
     Or,
