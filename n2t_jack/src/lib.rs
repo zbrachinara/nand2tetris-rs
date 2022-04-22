da5d8ceeabaec619f11b68c5301f8f45576a67e2
Eric Yu <zbrachinara@gmail.com>
2022-04-21 23:25:00 -0400

    hacked and relaxed `Copy` restrictions on `const_concat`

--- n2t_jack/src/lib.rs
+++ n2t_jack/src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
-#![feature(const_refs_to_cell)]
+#![feature(const_mut_refs)]
+#![feature(const_intrinsic_copy)]
 
 mod translate;
 mod util;
--- n2t_jack/src/util.rs
+++ n2t_jack/src/util.rs
@@ -4,20 +4,15 @@ use std::mem::MaybeUninit;
 /// Concatenates two arrays at compile time
 ///
 /// Kind of a hack, so it should be replaced as soon as this function is available in stdlib
-pub const fn const_concat<T: Clone, const N: usize, const M: usize>(
-    arr1: [T; N],
-    arr2: [T; M],
+pub const fn const_concat<T, const N: usize, const M: usize>(
+    arr1: &[T; N],
+    arr2: &[T; M],
 ) -> [T; N + M] {
-    let mut combined = unsafe { [MaybeUninit::uninit().assume_init(); N + M] };
-    let mut i = 0;
-    while i < arr1.len() {
-        combined[i] = arr1[i].clone();
-        i += 1;
+    unsafe {
+        let mut combined: [T; N + M] = MaybeUninit::uninit().assume_init();
+        std::ptr::copy_nonoverlapping(arr1.as_ptr(), combined.as_mut_ptr(), N);
+        std::ptr::copy_nonoverlapping(arr2.as_ptr(), combined.as_mut_ptr().add(N), M);
+        combined
     }
-    let mut j = 0;
-    while j < arr2.len() {
-        combined[i + j] = arr2[j].clone();
-        j += 1;
-    }
-    combined
+
 }
