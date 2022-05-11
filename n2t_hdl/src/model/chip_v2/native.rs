ce6ebb2043bfb5537e209b775c061bcade969f94
Eric Yu <zbrachinara@gmail.com>
2022-05-11 14:45:45 -0400

    add documentation for private structs

--- n2t_hdl/src/model/chip_v2/native.rs
+++ n2t_hdl/src/model/chip_v2/native.rs
@@ -6,12 +6,23 @@ struct Router {
     map: Vec<(ChannelRange, (Id, ChannelRange))>,
 }
 
+/// Represents a request to modify the chip represented by the id with the
+/// inside data
+/// 
+/// Inside the targeted chip, only the data specified by the range will be
+/// modified.
 struct Request<'data> {
     id: Id,
     data: &'data [bool],
     range: ChannelRange,
 }
 
+/// Barrier representing how the chip interacts inside a native chip
+/// 
+/// clock_mask is set true for bits which are not clocked. When a clock cycle
+/// occurs, all bits pass from in_buffer to out_buffer. But when a non-clock
+/// eval occurs, only those marked as true in clock_mask pass from in_buffer to
+/// out_buffer.
 struct Barrier {
     in_buffer: Vec<bool>,
     out_buffer: Vec<bool>,
