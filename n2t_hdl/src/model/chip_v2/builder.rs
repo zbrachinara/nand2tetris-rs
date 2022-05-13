ef6c218930abfc7464284de9cfa62c579679ea0b
Eric Yu <zbrachinara@gmail.com>
2022-05-13 19:30:39 -0400

    issue warning for adding builtin chips

--- n2t_hdl/src/model/chip_v2/builder.rs
+++ n2t_hdl/src/model/chip_v2/builder.rs
@@ -1,7 +1,8 @@
 use super::error::ModelConstructionError;
-use super::{builtin, Chip};
-use crate::model::parser::Chip as ChipRepr;
+use super::{builtin, Chip, Id};
+use crate::channel_range::ChannelRange;
 use crate::model::parser::Interface;
+use crate::model::parser::{Chip as ChipRepr, Form};
 use std::collections::HashMap;
 
 pub struct ChipBuilder {
@@ -20,21 +21,25 @@ impl ChipBuilder {
         }
     }
 
-    pub fn with_builtins(mut self) -> Self {
+    pub fn with_builtins(&mut self) {
         self.registered
             .extend([("Nand".to_string(), builtin::nand())]);
-        self
     }
 
     pub fn register_hdl(&mut self, chip: ChipRepr) -> Result<(), ModelConstructionError> {
-        let Interface {
+        let top_interface = chip.interface();
+        let ChipRepr {
             name,
-            com_in,
-            com_out,
-            seq_in,
-            seq_out,
-        } = chip.interface();
+            in_pins,
+            out_pins,
+            logic,
+        } = chip;
+        let top_router: Vec<(ChannelRange, (Id, ChannelRange))> = Vec::new();
 
-        todo!()
+        if let Form::Native(_) = logic {
+            todo!()
+        } else {
+            panic!("dynamic loading of native chips is not yet supported")
+        }
     }
 }
